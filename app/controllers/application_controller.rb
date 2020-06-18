26e199da876ac0a0363344860c46bf004bb45c27
dennis <dionisios.simoni@gmail.com>
2020-06-18 21:24:19 +0100

    Set up login form and add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    unless logged_in?
+      flash[:error] = 'Please login to perform this action!'
+      redirect_to login_path
+    end
+  end
 end
