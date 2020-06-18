f691be17065e08bd5815cd85638b06cc25cc879c
dennis <dionisios.simoni@gmail.com>
2020-06-18 23:00:41 +0100

    Colour flash messages and add restrictions to accessible pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,11 @@
 class SessionsController < ApplicationController
-
-  def new
-
-  end
+  before_action :already_logged_in?, only: [:new, :create]
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = 'Successfully logged in!'
+      flash[:success] = 'Successfully logged in!'
       redirect_to root_path
     else
       flash.now[:error] = 'Credentials were invalid. Please try again'
@@ -20,6 +17,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'You have logged out successfully'
     redirect_to login_path
+  end
+
+  private
+
+  def already_logged_in?
+    if logged_in?
+      flash[:notice] = "You are already logged in!"
+      redirect_to root_path
+    end
   end
 
 end
