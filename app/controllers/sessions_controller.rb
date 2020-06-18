9b1176edb7579a4f005cf5aeb0252f0f83fe0c07
dennis <dionisios.simoni@gmail.com>
2020-06-18 22:33:31 +0100

    implement authentication / login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,22 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Successfully logged in!'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Credentials were invalid. Please try again'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out successfully'
+    redirect_to login_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  # Root:
   root 'chatroom#index'
+
+  # Session:
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
