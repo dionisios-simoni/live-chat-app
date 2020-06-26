ea9e41a76b2c3e37cdbfea90138882e0ba0b80e4
dennis <dionisios.simoni@gmail.com>
2020-06-26 10:42:19 +0100

    send messages using the websockets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,20 @@ require("@rails/activestorage").start()
 require("channels")
 require("semantic-ui-sass")
 
+scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight)
+    }
+};
+
+submit_message = function() {
+    $('#message_body').on('keydown', function(e) {
+        if (e.keyCode == 13) {
+            $('button').click();
+            e.target.value = "";
+        };
+    });
+};
 $(document).on('turbolinks:load', function () {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
@@ -16,6 +30,8 @@ $(document).on('turbolinks:load', function () {
             .closest('.message')
             .transition('fade');
         });
+    scroll_bottom();
+    submit_message();
 });
 
 // Uncomment to copy all static images under ../images to the output folder and reference
